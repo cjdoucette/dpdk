c60bbe26ea126a3946dd49a4a5742d7d1f1d1951
Cody Doucette <doucette@bu.edu>
2016-06-03 21:19:17 +0000

    examples: add untested version of VLAN

--- examples/vlan/main.c
+++ examples/vlan/main.c
@@ -32,8 +32,10 @@
  */
 
 #include <stdint.h>
+#include <stdbool.h>
 #include <inttypes.h>
 #include <rte_eal.h>
+#include <rte_ether.h>
 #include <rte_ethdev.h>
 #include <rte_cycles.h>
 #include <rte_lcore.h>
@@ -46,7 +48,23 @@
 #define MBUF_CACHE_SIZE 250
 #define BURST_SIZE 32
 
-#define USE_HARDWARE_VLAN_STRIPPING 1
+#define VLAN_TAG 1000
+
+/*
+ * The code can ignore needing to strip or insert VLAN tags, because
+ * the hardware will take care of it. VLAN stripping and tagging can
+ * be configured separately, but this sample application enables
+ * both or disables both.
+ */
+#define USE_VLAN_HARDWARE_OFFLOAD 1
+
+/*
+ * If we know that we're going to insert an egress VLAN tag, we
+ * can keep the ingress VLAN tag while processing the packet and
+ * then just overwrite the tag at the end. This avoids copying
+ * memory back and forth.
+ */
+#define KEEP_AND_OVERWRITE_VLAN 1
 
 static const struct rte_eth_conf port_conf_default = {
 	.rxmode = { .max_rx_pkt_len = ETHER_MAX_LEN, },
@@ -54,19 +72,25 @@ static const struct rte_eth_conf port_conf_default = {
 
 static unsigned nb_ports;
 
-/*
- * TODO Is it better to enable/disable hardware VLAN stripping/insertion
- * using rte_eth_dev_set_vlan_offload() or rte_eth_dev_configure()? Can
- * either be used? Or do the individual queues on the ports need to be
- * configured for this using rte_eth_dev_set_vlan_strip_on_queue()?
- */
+static inline int
+vlan_id_is_invalid(uint16_t vlan_id)
+{
+	if (vlan_id < 4096)
+		return 0;
+	printf("Invalid vlan_id %d (must be < 4096)\n", vlan_id);
+	return 1;
+}
+
 static void
 rx_vlan_strip_set(uint8_t port_id, int on)
 {
         int diag;
-        int vlan_offload = rte_eth_dev_get_vlan_offload(port_id);
+        int vlan_offload;
 
-	printf("vlan_offload: %08x\n", vlan_offload);
+	if (!rte_eth_dev_is_valid_port(port_id))
+                return;
+
+        vlan_offload = rte_eth_dev_get_vlan_offload(port_id);
 
         if (on)
                 vlan_offload |= ETH_VLAN_STRIP_OFFLOAD;
@@ -77,9 +101,46 @@ rx_vlan_strip_set(uint8_t port_id, int on)
         if (diag < 0)
                 printf("rx_vlan_strip_set(port_pi=%d, on=%d) failed "
                "diag=%d\n", port_id, on, diag);
+}
 
-        vlan_offload = rte_eth_dev_get_vlan_offload(port_id);
-	printf("vlan_offload: %08x\n", vlan_offload);
+static void
+rx_vlan_strip_set_on_queue(uint8_t port_id, uint16_t queue_id, int on)
+{
+        int diag;
+
+	if (!rte_eth_dev_is_valid_port(port_id))
+                return;
+
+        diag = rte_eth_dev_set_vlan_strip_on_queue(port_id, queue_id, on);
+        if (diag < 0)
+                printf("rx_vlan_strip_set_on_queue(port_pi=%d, queue_id=%d, on=%d) failed "
+               "diag=%d\n", port_id, queue_id, on, diag);
+}
+
+static void
+tx_vlan_pvid_set(uint8_t port_id, uint16_t vlan_id, int on)
+{
+	if (!rte_eth_dev_is_valid_port(port_id))
+                return;
+
+        rte_eth_dev_set_vlan_pvid(port_id, vlan_id, on);
+}
+
+static void
+vlan_tpid_set(uint8_t port_id, enum rte_vlan_type vlan_type, uint16_t tp_id)
+{
+        int diag;
+
+	if (!rte_eth_dev_is_valid_port(port_id))
+                return;
+
+        diag = rte_eth_dev_set_vlan_ether_type(port_id, vlan_type, tp_id);
+        if (diag == 0)
+                return;
+
+        printf("tx_vlan_tpid_set(port_pi=%d, vlan_type=%d, tpid=%d) failed "
+               "diag=%d\n",
+               port_id, vlan_type, tp_id, diag);
 }
 
 /*
@@ -97,8 +158,16 @@ port_init(uint8_t port, struct rte_mempool *mbuf_pool)
 	if (port >= rte_eth_dev_count())
 		return -1;
 
-	if (USE_HARDWARE_VLAN_STRIPPING)
-		port_conf.rxmode.hw_vlan_strip = 0;
+	/*
+	 * TODO Is it better to enable/disable hardware VLAN stripping/insertion
+	 * using rte_eth_dev_configure() or below? Can either be used? The
+	 * insert options claim to only be available for i40e NICs.
+	 */
+	if (USE_VLAN_HARDWARE_OFFLOAD) {
+		port_conf.rxmode.hw_vlan_strip = 1;
+		port_conf.txmode.hw_vlan_insert_pvid = 1;
+		port_conf.txmode.pvid = VLAN_TAG;
+	}
 	retval = rte_eth_dev_configure(port, rx_rings, tx_rings, &port_conf);
 	if (retval != 0)
 		return retval;
@@ -133,10 +202,115 @@ port_init(uint8_t port, struct rte_mempool *mbuf_pool)
 
 	rte_eth_promiscuous_enable(port);
 
-	if (USE_HARDWARE_VLAN_STRIPPING)
-		rx_vlan_strip_set(port, 0);
+	/*
+	 * TODO Is it better to enable/disable hardware VLAN stripping/insertion
+	 * here or above? Can either be used? If here, which of the following
+	 * calls are necessary?
+	 */
+	if (USE_VLAN_HARDWARE_OFFLOAD) {
+		rx_vlan_strip_set(port, 1);
+		rx_vlan_strip_set_on_queue(port, 0, 1);
+		tx_vlan_pvid_set(port, VLAN_TAG, 1);
+		vlan_tpid_set(port, ETH_VLAN_TYPE_INNER, VLAN_TAG);
+	}
 
 	return 0;
+}
+
+static void
+tx_packets(const uint8_t port, struct rte_mbuf **bufs, const uint16_t n)
+{
+	const uint16_t nb_tx = rte_eth_tx_burst(port, 0, bufs, n);
+	if (unlikely(nb_tx < n)) {
+		uint16_t buf;
+		for (buf = nb_tx; buf < n; buf++)
+			rte_pktmbuf_free(bufs[buf]);
+	}
+}
+
+static inline size_t
+get_vlan_offset(struct ether_hdr *eth_hdr, uint16_t *proto)
+{
+	size_t vlan_offset = 0;
+
+	if (rte_cpu_to_be_16(ETHER_TYPE_VLAN) == *proto) {
+		struct vlan_hdr *vlan_hdr = (struct vlan_hdr *)(eth_hdr + 1);
+
+		vlan_offset = sizeof(struct vlan_hdr);
+		*proto = vlan_hdr->eth_proto;
+
+		if (rte_cpu_to_be_16(ETHER_TYPE_VLAN) == *proto) {
+			vlan_hdr = vlan_hdr + 1;
+
+			*proto = vlan_hdr->eth_proto;
+			vlan_offset += sizeof(struct vlan_hdr);
+		}
+	}
+	return vlan_offset;
+}
+
+static void
+process_vlan(const uint8_t port, struct rte_mbuf **mbufs, const uint16_t nb_rx)
+{
+	struct ether_hdr *eth_hdr;
+	uint16_t ether_type, offset, i;
+	struct rte_mbuf *tx[nb_rx];
+	uint16_t nb_tx = 0;
+
+	for (i = 0; i < nb_rx; i++) {
+		bool vlan_present = false;
+
+		eth_hdr = rte_pktmbuf_mtod(mbufs[i], struct ether_hdr *);
+		ether_type = eth_hdr->ether_type;
+		if (ether_type == rte_cpu_to_be_16(ETHER_TYPE_VLAN)) {
+			vlan_present = true;
+			if (!KEEP_AND_OVERWRITE_VLAN) {
+				if (rte_vlan_strip(mbufs[i]) != 0) {
+					rte_pktmbuf_free(mbufs[i]);
+					continue;
+				}
+			}
+		}
+
+		/* Read in next ether_type after VLAN, if present. */
+		offset = get_vlan_offset(eth_hdr, &ether_type);
+
+		/* Process rest packet in some way. */
+		switch (rte_be_to_cpu_16(ether_type)) {
+		case ETHER_TYPE_ARP: {
+			struct arp_hdr *arp_hdr = (struct arp_hdr *)
+				((char *)(eth_hdr + 1) + offset);
+			(void)arp_hdr;
+			/* ... */
+			break;
+		}
+		/* ... */
+		default:
+			break;
+		}
+
+		/* Add VLAN tag by insertion or overwriting. */
+		if (vlan_present) {
+			if (KEEP_AND_OVERWRITE_VLAN) {
+				/* Overwrite old VLAN tag with new one. */
+				struct vlan_hdr *vlan_hdr;
+				vlan_hdr = (struct vlan_hdr *)(eth_hdr + 1);
+				vlan_hdr->vlan_tci = rte_cpu_to_be_16(VLAN_TAG);
+			} else {
+				/* Insert VLAN tag. */
+				mbufs[i]->ol_flags |= PKT_TX_VLAN_PKT;
+				mbufs[i]->vlan_tci = VLAN_TAG;
+				if (rte_vlan_insert(&mbufs[i]) != 0) {
+					rte_pktmbuf_free(mbufs[i]);
+					continue;
+				}
+			}
+		}
+
+		tx[nb_tx++] = mbufs[i];
+	}
+
+	tx_packets(port, tx, nb_tx);
 }
 
 /*
@@ -163,17 +337,27 @@ lcore_main(void)
 			struct rte_mbuf *bufs[BURST_SIZE];
 			const uint16_t nb_rx = rte_eth_rx_burst(port, 0,
 					bufs, BURST_SIZE);
-			uint16_t buf;
 
 			if (unlikely(nb_rx == 0))
 				continue;
 
-			for (buf = 0; buf < nb_rx; buf++) {
-				struct rte_mbuf *mbuf = bufs[buf];
-				unsigned int len = rte_pktmbuf_data_len(mbuf);
-				rte_pktmbuf_dump(stdout, mbuf, len);
-				rte_pktmbuf_free(mbuf);
-			}
+			if (USE_VLAN_HARDWARE_OFFLOAD)
+				/*
+				 * You could do some processing on the
+				 * packets first, but then just transmit them.
+				 * The hardware will take care of removing
+				 * the VLAN tag before we see the packets, and
+				 * will add a tag after we're done.
+				 */
+				tx_packets(port, bufs, nb_rx);
+			else
+				/* Do some processing on the packets while
+				 * keeping the VLAN header in mind. We can
+				 * either remove the VLAN header before
+				 * processing, or keep it in and then
+				 * overwrite it with a new tag before tx.
+				 */
+				process_vlan(port, bufs, nb_rx);
 		}
 	}
 }
@@ -192,6 +376,9 @@ main(int argc, char *argv[])
 		rte_exit(EXIT_FAILURE, "Error with EAL initialization\n");
 	argc -= ret;
 	argv += ret;
+
+	if (vlan_id_is_invalid(VLAN_TAG))
+		rte_exit(EXIT_FAILURE, "Error with VLAN tag\n");
 
 	nb_ports = rte_eth_dev_count();
 	if (nb_ports < 2 || (nb_ports & 1))
