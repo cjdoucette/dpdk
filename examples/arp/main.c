197b02debfc04b0dc53516f3bec2f2d1bd39b1ee
Cody Doucette <doucette@bu.edu>
2016-06-04 05:40:22 +0000

    examples: add mempool and hash for ARP cache

--- examples/arp/main.c
+++ examples/arp/main.c
@@ -40,6 +40,7 @@
 #include <rte_cycles.h>
 #include <rte_lcore.h>
 #include <rte_mbuf.h>
+#include <rte_hash.h>
 
 #define RX_RING_SIZE 128
 #define TX_RING_SIZE 512
@@ -48,11 +49,13 @@
 #define MBUF_CACHE_SIZE 250
 #define BURST_SIZE 32
 
+
 static const struct rte_eth_conf port_conf_default = {
 	.rxmode = { .max_rx_pkt_len = ETHER_MAX_LEN, },
 };
 
 static unsigned nb_ports;
+
 /* TODO: use ring instead. */
 static uint8_t num_ip_addrs[5];
 static uint32_t ip_addrs[5][5];
@@ -119,6 +122,146 @@ port_init(uint8_t port, struct rte_mempool *mbuf_pool)
 	return 0;
 }
 
+/*
+ *	ARP cache
+ */
+
+/* TODO: add this to config file. */
+
+struct arp_cache_entry {
+	struct ether_addr	ha;
+	uint64_t		ts;
+};
+
+#ifdef RTE_MACHINE_CPUFLAG_SSE4_2
+#include <rte_hash_crc.h>
+#define DEFAULT_HASH_FUNC       rte_hash_crc
+#else
+#include <rte_jhash.h>
+#define DEFAULT_HASH_FUNC       rte_jhash
+#endif
+
+#define ARP_CACHE_ENTRIES 1024
+#define ARP_MEMPOOL_CACHE_SIZE 73
+static struct rte_hash_parameters arp_cache_params = {
+	.name = "arp_cache",
+	.entries = ARP_CACHE_ENTRIES,
+	.reserved = 0,
+	.key_len = sizeof(uint32_t),
+	.hash_func = DEFAULT_HASH_FUNC,
+	.hash_func_init_val = 0,
+	.socket_id = 0,
+	.extra_flag = 0,
+};
+
+static struct rte_hash *arp_cache = NULL;
+static struct rte_mempool *arp_cache_entry_pool;
+
+static void
+arp_cache_entry_init(struct rte_mempool *mp,
+		     __attribute__((unused)) void *opaque_arg,
+		     void *_m,
+		     __attribute__((unused)) unsigned i)
+{
+	struct rte_mbuf *m = _m;
+	uint32_t arp_cache_entry_size, buf_len, priv_size;
+
+	priv_size = rte_pktmbuf_priv_size(mp);
+	arp_cache_entry_size = sizeof(struct arp_cache_entry);
+	buf_len = rte_pktmbuf_data_room_size(mp);
+
+	RTE_ASSERT(priv_size == 0);
+	RTE_ASSERT(mp->elt_size >= arp_cache_entry_size);
+	(void)arp_cache_entry_size;
+
+	/* start of buffer is after mbuf structure and priv data */
+	m->priv_size = priv_size;
+	m->buf_addr = (char *)m;
+	m->buf_physaddr = rte_mempool_virt2phy(mp, m);
+	m->buf_len = (uint16_t)buf_len;
+
+	/* No need for room between start of buffer and data. */
+	m->data_off = 0;
+
+	/* Initialize some constant fields. */
+	m->pool = mp;
+	m->nb_segs = 1;
+	m->port = 0xff;
+}
+
+static int
+init_arp_cache(void)
+{
+	arp_cache_params.socket_id = rte_socket_id();
+	arp_cache = rte_hash_create(&arp_cache_params);
+	if (arp_cache == NULL)
+		return -1;
+
+	/*
+	 * Size of mempool should be n = (power of 2) - 1, and the size
+	 * of the mempool cache should be c such that n % c == 0.
+	 */
+	arp_cache_entry_pool = rte_mempool_create("arp_pool",
+		ARP_CACHE_ENTRIES / 2 - 1, sizeof(struct arp_cache_entry),
+		ARP_MEMPOOL_CACHE_SIZE, 0,
+		NULL, NULL,
+		arp_cache_entry_init, NULL,
+		rte_socket_id(), 0);
+	if (arp_cache_entry_pool == NULL) {
+		rte_hash_free(arp_cache);
+		return -1;
+	}
+
+	/*
+	 * TODO: pre-load cache with entries from config file.
+	 * See examples/ipv4_multicast/main.c for an example.
+	 */
+
+	return 0;
+}
+
+/* Not multi-thread safe. */
+static int
+arp_cache_put(const uint32_t ip, const struct ether_addr *ha)
+{
+	struct arp_cache_entry *arp_entry;
+	uint64_t update_time;
+	int ret;
+
+	update_time = 0;
+
+	ret = rte_hash_lookup_data(arp_cache, &ip, (void **)&arp_entry);
+	if (ret == 0)
+		arp_entry->ts = update_time;
+	else if (ret == -ENOENT) {
+		ret = rte_mempool_sc_get(arp_cache_entry_pool,
+					 (void **)&arp_entry);
+		if (ret == -ENOENT) {
+			printf("\nWARNING: no memory left in ARP cache entry pool; ARP entry not added\n");
+			return -1;
+		}
+		arp_entry->ts = update_time;
+		ether_addr_copy(ha, &arp_entry->ha);
+		ret = rte_hash_add_key_data(arp_cache, &ip, arp_entry);
+		if (ret != 0) {
+			printf("\nWARNING: invalid parameters; ARP entry not added\n");
+			rte_mempool_sp_put(arp_cache_entry_pool, arp_entry);
+			return -1;
+		}
+	} else {
+		printf("\nWARNING: invalid parametesr; could not lookup ARP entry\n");
+		return -1;
+	}
+
+	return 0;
+}
+
+static inline const struct ether_addr *
+arp_cache_get(__attribute__((unused)) const uint32_t ip)
+{
+	return NULL;
+}
+
 static bool
 port_has_ip(const uint8_t port, const uint32_t ip)
 {
@@ -138,23 +281,25 @@ static bool
 process_arp(uint8_t port, struct ether_hdr *eth_hdr, struct arp_hdr *arp_hdr,
 	    struct ether_addr *port_ether_addr)
 {
-	/*
-	 * We only support ARP requests for resolving
-	 * IPv4 addresses to Ethernet addresses.
-	 */
-	if (arp_hdr->arp_hrd != rte_cpu_to_be_16(ARP_HRD_ETHER) ||
-	    arp_hdr->arp_pro != rte_cpu_to_be_16(ETHER_TYPE_IPv4) ||
-	    arp_hdr->arp_hln != ETHER_ADDR_LEN ||
-	    arp_hdr->arp_pln != sizeof(uint32_t))
+	uint32_t target_ip_be_32;
+
+	/* We only support ARP for resolving IPv4 --> Ethernet addresses. */
+	if (unlikely(arp_hdr->arp_hrd != rte_cpu_to_be_16(ARP_HRD_ETHER) ||
+		     arp_hdr->arp_pro != rte_cpu_to_be_16(ETHER_TYPE_IPv4) ||
+		     arp_hdr->arp_hln != ETHER_ADDR_LEN ||
+		     arp_hdr->arp_pln != sizeof(uint32_t)))
 		return false;
 
-	switch (rte_be_to_cpu_16(arp_hdr->arp_op)) {
-	case ARP_OP_REQUEST: {
-		/* Check if we have this IP address. */
-		uint32_t target_ip_be_32 = arp_hdr->arp_data.arp_tip;
-		if (!port_has_ip(port, rte_be_to_cpu_32(target_ip_be_32)))
-			return false;
+	/* Check if we have this IP address. */
+	target_ip_be_32 = arp_hdr->arp_data.arp_tip;
+	if (!port_has_ip(port, rte_be_to_cpu_32(target_ip_be_32)))
+		return false;
 
+	/* Update cache with source resolution, regardless of operation. */
+	arp_cache_put(arp_hdr->arp_data.arp_sip, &arp_hdr->arp_data.arp_sha);
+
+	switch (rte_be_to_cpu_16(arp_hdr->arp_op)) {
+	case ARP_OP_REQUEST:
 		/* Set-up Ethernet header. */
 		ether_addr_copy(&eth_hdr->s_addr, &eth_hdr->d_addr);
 		ether_addr_copy(port_ether_addr, &eth_hdr->s_addr);
@@ -167,12 +312,18 @@ process_arp(uint8_t port, struct ether_hdr *eth_hdr, struct arp_hdr *arp_hdr,
 		ether_addr_copy(port_ether_addr, &arp_hdr->arp_data.arp_sha);
 		arp_hdr->arp_data.arp_sip = target_ip_be_32;
 		return true;
-	}
 	case ARP_OP_REPLY:
+		/*
+		 * No further action required. Could check to make sure
+		 * arp_hdr->arp_data.arp_tha is equal to port_ether_addr,
+		 * but there's nothing that can be done if it's wrong anyway.
+		 */
 		return false;
 	default:
 		return false;
 	}
+
+	RTE_ASSERT(false);
 }
 
 static inline size_t
@@ -205,11 +356,6 @@ process_rx(uint8_t port, struct rte_mbuf **mbufs, const uint16_t nb_rx)
 		struct ether_hdr *eth_hdr;
 		uint16_t ether_type, offset;
 
-#ifdef DEBUG
-		unsigned int len = rte_pktmbuf_data_len(mbufs[i]);
-		rte_pktmbuf_dump(stdout, mbufs[i], len);
-#endif
-
 		eth_hdr = rte_pktmbuf_mtod(mbufs[i], struct ether_hdr *);
 
 		/*
@@ -225,16 +371,15 @@ process_rx(uint8_t port, struct rte_mbuf **mbufs, const uint16_t nb_rx)
 
 		/* Calculate offset to skip over VLAN header, if present. */
 		ether_type = eth_hdr->ether_type;
-                offset = get_vlan_offset(eth_hdr, &ether_type);
+		offset = get_vlan_offset(eth_hdr, &ether_type);
 
 		switch (rte_be_to_cpu_16(ether_type)) {
 		case ETHER_TYPE_ARP: {
 			struct arp_hdr *arp_hdr = (struct arp_hdr *)
 				((char *)(eth_hdr + 1) + offset);
 
-			bool need_tx = process_arp(port, eth_hdr, arp_hdr,
-						      &port_ether_addr);
-			if (need_tx) {
+			if (process_arp(port, eth_hdr, arp_hdr,
+					&port_ether_addr)) {
 				const uint16_t nb_tx = rte_eth_tx_burst(port,
 						0, &mbufs[i], 1);
 				if (unlikely(nb_tx < 1))
@@ -316,6 +461,10 @@ main(int argc, char *argv[])
 		if (port_init(portid, mbuf_pool) != 0)
 			rte_exit(EXIT_FAILURE, "Cannot init port %"PRIu8"\n",
 					portid);
+
+	ret = init_arp_cache();
+	if (ret != 0)
+		rte_exit(EXIT_FAILURE, "Cannot build the ARP cache\n");
 
 	if (rte_lcore_count() > 1)
 		printf("\nWARNING: Too much enabled lcores - "
