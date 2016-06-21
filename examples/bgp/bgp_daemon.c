3b14174ac953177bd83f837093d6ff427a107385
Cody Doucette <doucette@bu.edu>
2016-06-21 21:44:46 +0000

    examples: add KNI to BGP and update dummy daemon

--- examples/bgp/bgp_daemon.c
+++ examples/bgp/bgp_daemon.c
@@ -5,32 +5,39 @@
 #include <netinet/tcp.h>
 #include <unistd.h>
 
-#define BUF_SIZE	128
+#define BUF_SIZE	1500
+#define TCP_BGP_PORT	179
 
 int
 main(void)
 {
 	unsigned char buf[BUF_SIZE];
+	struct tcphdr *tcph;
 	unsigned char *data;
 	ssize_t numbytes;
 	int s;
 
-	s = socket(PF_INET, SOCK_RAW, IPPROTO_TCP);
+	s = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
 	if (s == -1) {
 		perror("socket");
 		return -1;
 	}
 
-	memset(buf, 0, BUF_SIZE);
-	numbytes = recvfrom(s, buf, BUF_SIZE, 0, NULL, NULL);
-	if (numbytes < 0) {
-		perror("recvfrom");
-		close(s);
-		return -1;
-	}
+	while (1) {
+		memset(buf, 0, BUF_SIZE);
+		numbytes = recvfrom(s, buf, BUF_SIZE, 0, NULL, NULL);
+		if (numbytes < 0) {
+			perror("recvfrom");
+			close(s);
+			return -1;
+		}
+		tcph = (struct tcphdr *)(buf + sizeof(struct ip));
+		data = buf + sizeof(struct iphdr) + sizeof(struct tcphdr);
 
-	data = buf + sizeof(struct iphdr) + sizeof(struct tcphdr);
-	printf("%s\n", data);
+		if (ntohs(tcph->dest) != 22)
+			printf("%zd bytes received (%hu): %s\n", numbytes,
+				ntohs(tcph->dest), data);
+	}
 
 	close(s);
 	return 0;
